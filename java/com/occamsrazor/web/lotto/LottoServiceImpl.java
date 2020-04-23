95473e47153ddba4a931a1fc26f0d7693f047903
leejua1 <1479q@naver.com>
2020-04-23 10:26:09 +0900

    0423 10:26

--- java/com/occamsrazor/web/lotto/LottoServiceImpl.java
+++ java/com/occamsrazor/web/lotto/LottoServiceImpl.java
@@ -1,6 +1,8 @@
 package com.occamsrazor.web.lotto;
 
 
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
 import com.occamsrazor.web.util.Prize;
@@ -26,10 +28,59 @@ public LottoServiceImpl() {
 
 	@Override
 	public Prize detail(String userid) {
-	
-	
-		return null;
-	}
+	Prize prize = null;
+	switch(lottocount(userid)) {
+	case 0: prize = Prize.꽝; break;
+	case 1: prize = Prize.오등; break;
+	case 2: prize = Prize.사등; break;
+	case 3: prize = Prize.삼등; break;
+	case 4: prize = Prize.이등; break;
+	case 5: prize = Prize.일등; break;
 
+		
+	}
+	
+		return prize;
+	}
+	private int lottocount(String userid) {
+		Random r = new Random();
+		int lottoCount=0;
+		int[] randomLotto=new int[5];
+		for (int i = 0; i < count; i++) {
+			if (userid.equals(lottos[i].getUserid())) {
+				for (int j = 0; j < 5; j++) {
+					randomLotto[j] = r.nextInt(5)+1;
+					System.out.println("랜덤 로또 번호 "+j + "번째:" +randomLotto[j]);
+				
+						}
+				if (randomLotto[0]==Integer.parseInt(lottos[i].getFirst())) {
+					lottoCount++;
+
+				}
+				if(randomLotto[1]==Integer.parseInt(lottos[i].getSecond())){
+					lottoCount++;
+				}
+				if(randomLotto[2]==Integer.parseInt(lottos[i].getThird())) {
+					lottoCount++;
+				}
+				if(randomLotto[3]==Integer.parseInt(lottos[i].getForth())) {
+					lottoCount++;	
+				}
+				if(randomLotto[4]==Integer.parseInt(lottos[i].getFifth())) {
+					lottoCount++;
+					}
+				break;
+				
+				
+				}
+			
+			
+				}
+			
+		
+			
+			System.out.println("맞은 개수 : "+ lottoCount);
+		return lottoCount;
+	}
 
 }
--- java/com/occamsrazor/web/util/Prize.java
+++ java/com/occamsrazor/web/util/Prize.java
@@ -1,5 +1,5 @@
 package com.occamsrazor.web.util;
 
 public enum Prize {
-일등,이등,삼등,사등,오등;
+꽝,일등,이등,삼등,사등,오등;
 }
